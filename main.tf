60113ad80694c6793240688abbe761df8f10c32f
Adam Retter <adam.retter@googlemail.com>
2021-08-22 17:49:22 +0200

    Further improvements to the Terraform script

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.54.0"
+      version = "~> 3.55.0"
     }
   }
 
@@ -18,50 +18,207 @@ provider "aws" {
   region  = "eu-west-2"
 }
 
+resource "aws_eip" "nat" {
+  count = 1
+  vpc   = true
+}
 
-# TODO(AR) IPv6 block
-resource "aws_vpc" "tna_ct_omega_vpc" {
-  cidr_block = "10.11.0.0/16"
+resource "aws_customer_gateway" "tna_ct_omega_gateway" {
+  bgp_asn    = 65000     # TODO(AR) need the ASN for TNA
+  ip_address = "1.1.3.4" # TODO(AR) need a public IP for TNA
+  type       = "ipsec.1"
+
   tags = {
-	Name = "vpc"
+    Name = "vpn-gateway"
   }
 }
 
-# TODO(AR) IPv6 subnet
-resource "aws_subnet" "dev_subnet" {
-  vpc_id     = aws_vpc.tna_ct_omega_vpc.id
-  cidr_block = "10.11.10.0/24"
+module "vpn_gateway" {
+  source = "terraform-aws-modules/vpn-gateway/aws"
+
+  vpn_gateway_id      = module.vpc.vgw_id
+  customer_gateway_id = aws_customer_gateway.tna_ct_omega_gateway.id
+
+  vpc_id                       = module.vpc.vpc_id
+  vpc_subnet_route_table_ids   = module.vpc.private_route_table_ids
+  vpc_subnet_route_table_count = length(module.vpc.private_subnets)
+  local_ipv4_network_cidr      = "0.0.0.0/0"
+  remote_ipv4_network_cidr     = module.vpc.vpc_cidr_block # TODO(AR) should we restrict to just private subnet?
+}
+
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "tna_ct_omega_vpc"
+
+  cidr = "10.128.238.0/24"
+
+  private_subnets = ["10.128.238.0/29"]   # dev_private_subnet
+  intra_subnets   = ["10.128.238.248/29"] # dev_internal_subnet
+
+  enable_ipv6                     = true
+  assign_ipv6_address_on_creation = true
+  private_subnet_ipv6_prefixes    = [0]
+  intra_subnet_ipv6_prefixes      = [16]
+
+  enable_nat_gateway  = true
+  single_nat_gateway  = false
+  reuse_nat_ips       = true             # <= Skip creation of EIPs for the NAT Gateways
+  external_nat_ip_ids = aws_eip.nat.*.id # <= IPs specified here as input to the module
+
+  enable_vpn_gateway = true
 
   tags = {
-    Name = "subnet"
+    Name = "vpc"
+  }
+}
+
+data "aws_ami" "amazon_linux_2" {
+
+  # Likely to be AMI with id: ami-0d26eb3972b7f8c96
+
+  most_recent = true
+
+  owners = ["137112412989"] # Amazon Web Services
+
+  filter {
+    name   = "public"
+    values = ["true"]
+  }
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+}
+
+module "dev_workstation_secuirty_group" {
+  source = "terraform-aws-modules/security-group/aws"
+
+  name        = "dev_workstation_security_group"
+  description = "Security group for Development Workstation ports open within VPC"
+
+  vpc_id = module.vpc.vpc_id
+
+  ingress_with_cidr_blocks = [
+    {
+      description = "SSH"
+      from_port   = 0
+      to_port     = 22
+      protocol    = "tcp"
+      cidr_blocks = module.vpc.private_subnets_cidr_blocks
+    },
+    {
+      description = "RDP"
+      from_port   = 0
+      to_port     = 3389
+      protocol    = "tcp"
+      cidr_blocks = module.vpc.private_subnets_cidr_blocks
+    }
+  ]
+
+  ingress_with_ipv6_cidr_blocks = [
+    {
+      description      = "SSH (IPv6)"
+      from_port        = 0
+      to_port          = 22
+      protocol         = "tcp"
+      ipv6_cidr_blocks = module.vpc.private_subnets_ipv6_cidr_blocks
+    },
+    {
+      description = "RDP (IPv6)"
+      from_port   = 0
+      to_port     = 3389
+      protocol    = "tcp"
+      cidr_blocks = module.vpc.private_subnets_ipv6_cidr_blocks
+    }
+  ]
+
+  egress_with_cidr_blocks = [
+    {
+      description = "All"
+      from_port   = 0
+      to_port     = 0
+      protocol    = 0
+      cidr_blocks = "0.0.0.0/0"
+    }
+  ]
+
+  egress_with_ipv6_cidr_blocks = [
+    {
+      description = "All"
+      from_port   = 0
+      to_port     = 0
+      protocol    = 0
+      cidr_blocks = "2001:db8::/64"
+    }
+  ]
+
+  tags = {
+    Name        = "security_group"
     Environment = "dev"
   }
 }
 
-# TODO(AR) IPv6 addresses
-resource "aws_network_interface" "dev_workstation_1" {
-  subnet_id   = aws_subnet.dev_subnet.id
-  private_ips = ["10.11.10.11"]
+resource "aws_network_interface" "dev_workstation_1_private_interface" {
+  description        = "Private Subnet Interface for Dev Workstation 1"
+  subnet_id          = module.vpc.private_subnets[0].id
+  private_ips        = ["10.128.238.0"]
+  ipv6_address_count = 0 # use assign_ipv6_address_on_creation=true from the vpc subnet configuration
 
   tags = {
     Name        = "primary_network_interface"
+    Network     = "private"
     Environment = "dev"
   }
 }
 
-# TODO(AR) - block devices?
-# TODO(AR) - how about scheduled startup times each day?
-# TODO(AR) - AWS firewall?
-# TODO(AR) - separate interface for database access only?
-# TODO(AR) - how to initiate Puppet?
+resource "aws_network_interface" "dev_workstation_1_internal_interface" {
+  description        = "Internal Subnet Interface for Dev Workstation 1"
+  subnet_id          = module.vpc.intra_subnets[0].id
+  private_ips        = ["10.128.238.248"]
+  ipv6_address_count = 0 # use assign_ipv6_address_on_creation=true from the vpc subnet configuration
+
+  tags = {
+    Name        = "secondary_network_interface"
+    Network     = "internal"
+    Environment = "dev"
+  }
+}
+
 resource "aws_instance" "dev_workstation_1" {
-  ami           = "ami-0d26eb3972b7f8c96"
+  ami           = data.aws_ami.amazon_linux_2.id
   instance_type = "m5a.2xlarge"
 
+  monitoring = false
+
   network_interface {
-    network_interface_id = aws_network_interface.dev_workstation_1.id
+    network_interface_id = aws_network_interface.dev_workstation_1_private_interface.id
     device_index         = 0
   }
+
+  network_interface {
+    network_interface_id = aws_network_interface.dev_workstation_1_internal_interface.id
+    device_index         = 1
+  }
+
+  root_block_device {
+    delete_on_termination = false
+    encrypted             = false
+    volume_type           = "gp3"
+    iops                  = 3000
+    throughput            = 120 # 120 MiB/s == ~125 MBps
+    volume_size           = 500 # GiB
+
+    tags = {
+      Name        = "primary_volume"
+      Environment = "dev"
+    }
+  }
+
+  security_groups = [
+    "dev_workstation_secuirty_group"
+  ]
 
   tags = {
     Name        = "dev_workstation"
@@ -69,5 +226,99 @@ resource "aws_instance" "dev_workstation_1" {
   }
 }
 
-# TODO(AR) - SQLServer VM - doesn't need as much power as dev VMs
+resource "aws_network_interface" "dev_mssql_server_1_internal_interface" {
+  description        = "Internal Subnet Interface for Dev MS SQL Server 1"
+  subnet_id          = module.vpc.intra_subnets[0].id
+  private_ips        = ["10.128.238.255"]
+  ipv6_address_count = 0 # use assign_ipv6_address_on_creation=true from the vpc subnet configuration
+
+  tags = {
+    Name        = "primary_network_interface"
+    Network     = "internal"
+    Environment = "dev"
+  }
+}
+
+resource "aws_instance" "mssql_server_1" {
+  ami           = data.aws_ami.amazon_linux_2.id
+  instance_type = "r5.xlarge"
+  # m5a.2xlarge == $0.4 / hour == 8 vCPU == 32GiB RAM
+  # r5.xlarge == $0.296 / hour == 4 vCPU == 32GiB RAM
+
+  monitoring = true
+
+  network_interface {
+    network_interface_id = aws_network_interface.dev_mssql_server_1_internal_interface.id
+    device_index         = 0
+  }
+
+  root_block_device {
+    delete_on_termination = false
+    encrypted             = false
+    volume_type           = "gp3"
+    iops                  = 3000
+    throughput            = 120 # 120 MiB/s == ~125 MBps
+    volume_size           = 60  # GiB
+
+    tags = {
+      Name        = "primary_volume"
+      Environment = "dev"
+    }
+  }
+
+  ebs_block_device {
+    device_name           = "xvdb"
+    delete_on_termination = false
+    encrypted             = false
+    volume_type           = "gp3" # TODO(AR) consider upgrading to io2 and/or more IOPS
+    iops                  = 3000
+    throughput            = 120 # 120 MiB/s == ~125 MBps
+    volume_size           = 150 # GiB
+
+    tags = {
+      Name        = "mssql_data_volume"
+      Environment = "dev"
+    }
+  }
+
+  ebs_block_device {
+    device_name           = "xvdc"
+    delete_on_termination = false
+    encrypted             = false
+    volume_type           = "gp3" # TODO(AR) consider upgrading to io2 and/or more IOPS
+    iops                  = 3000
+    throughput            = 120 # 120 MiB/s == ~125 MBps
+    volume_size           = 75  # GiB
+
+    tags = {
+      Name        = "mssql_log_volume"
+      Environment = "dev"
+    }
+  }
+
+  ebs_block_device {
+    device_name           = "xvdd"
+    delete_on_termination = false
+    encrypted             = false
+    volume_type           = "gp3" # TODO(AR) consider upgrading to io2 and/or more IOPS
+    iops                  = 3000
+    throughput            = 120 # 120 MiB/s == ~125 MBps
+    volume_size           = 150 # GiB
+
+    tags = {
+      Name        = "mssql_backup_volume"
+      Environment = "dev"
+    }
+  }
+
+  tags = {
+    Name        = "dev_mssql_server"
+    Environment = "dev"
+  }
+}
+
+
+# TODO(AR) - how about scheduled startup times each day? -- AWS Instance Scheduler -- https://registry.terraform.io/modules/diodonfrost/lambda-scheduler-stop-start/aws/latest
+
+# TODO(AR) - how to initiate Puppet?
 
