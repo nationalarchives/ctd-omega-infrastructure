35a368869fe58e1eb37cf8d3eb85a371933f1e58
Adam Retter <adam.retter@googlemail.com>
2021-08-23 10:31:38 +0200

    Remove invalid filter

--- main.tf
+++ main.tf
@@ -91,11 +91,6 @@ data "aws_ami" "amazon_linux_2" {
   owners = ["137112412989"] # Amazon Web Services
 
   filter {
-    name   = "public"
-    values = ["true"]
-  }
-
-  filter {
     name   = "name"
     values = ["amzn2-ami-hvm-*-x86_64-gp2"]
   }
