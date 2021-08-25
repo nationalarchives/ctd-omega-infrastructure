c38ac4b19e94859db5c50488af5624d7c52ab76f
Adam Retter <adam.retter@googlemail.com>
2021-08-25 13:33:54 +0200

    Use Client Side VPN not Site-to-site

--- main.tf
+++ main.tf
@@ -23,33 +23,176 @@ provider "aws" {
   region  = local.aws_region
 }
 
-resource "aws_eip" "nat" {
-  count = 1
-  vpc   = true
-}
+# TODO(AR) is this still needed
+#resource "aws_eip" "nat" {
+#  count = 1
+#  vpc   = true
+#}
 
-resource "aws_customer_gateway" "tna_ct_omega_gateway" {
-  bgp_asn    = 65000     # TODO(AR) need the ASN for TNA
-  ip_address = "1.1.3.4" # TODO(AR) need a public IP for TNA
-  type       = "ipsec.1"
+
+# TODO - Security Group for VPN -- should be SSH and RDP
+# TODO - NACL (Network ACLs) Rules for VPN -- in from SSH/RDP -- out from ALL
+# TODO - Certificate Manager - for VPN
+#  --- Need a Root Chain cert
+#  --- Need a Cert per User
+#  --- AWS will provide OVPN files for users
+
+resource "aws_acm_certificate" "vpn_server" {
+  domain_name = "omega.nationalarchives.com"
+  validation_method = "DNS"
+
+  lifecycle {
+    create_before_destroy = true
+  }
 
   tags = {
-    Name = "vpn-gateway"
+    Name = "certificate"
+    Scope = "vpn_server"
+    Environment = "vpn"
   }
 }
 
-module "vpn_gateway" {
-  source = "terraform-aws-modules/vpn-gateway/aws"
-  version = "2.11.0"
+resource "aws_acm_certificate_validation" "vpn_server" {
+  certificate_arn = aws_acm_certificate.vpn_server.arn
+  timeouts {
+    create = "1m"
+  }
+}
 
-  vpn_gateway_id      = module.vpc.vgw_id
-  customer_gateway_id = aws_customer_gateway.tna_ct_omega_gateway.id
+resource "aws_acm_certificate" "vpn_client_root" {
+  #private_key = file("certs/client-vpn-ca.key")
+  #certificate_body = file("certs/client-vpn-ca.crt")
+  #certificate_chain = file("certs/ca-chain.crt")
 
-  vpc_id                       = module.vpc.vpc_id
-  vpc_subnet_route_table_ids   = module.vpc.private_route_table_ids
-  vpc_subnet_route_table_count = length(module.vpc.private_subnets)
-  local_ipv4_network_cidr      = "0.0.0.0/0"
-  remote_ipv4_network_cidr     = module.vpc.vpc_cidr_block # TODO(AR) should we restrict to just private subnet?
+  domain_name = "omega.nationalarchives.com"
+  validation_method = "DNS"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+
+  tags = {
+    Name = "certificate"
+    Scope = "vpn_client_root"
+    Environment = "vpn"
+  }
+}
+
+resource "aws_acm_certificate_validation" "vpn_client_root" {
+  certificate_arn = aws_acm_certificate.vpn_client_root.arn
+  timeouts {
+    create = "1m"
+  }
+}
+
+module "vpn_access_security_group" {
+  source = "terraform-aws-modules/security-group/aws"
+  version = "4.3.0"
+
+  name        = "dev_workstation_security_group"
+  description = "Security group for Development Workstation ports open within VPC"
+
+  vpc_id = module.vpc.vpc_id
+
+  computed_ingress_with_cidr_blocks = [
+    {
+      description = "Incoming VPN connection"
+      from_port   = 443
+      to_port     = 443
+      protocol    = "udp"
+      #cidr_blocks = "${module.vpc.private_subnets_cidr_blocks}"    # TODO(AR) why can't we use private subnet here?
+      cidr_blocks = module.vpc.vpc_cidr_block
+    }
+  ]
+  number_of_computed_ingress_with_cidr_blocks = 1
+
+  computed_ingress_with_ipv6_cidr_blocks = [
+    {
+      description      = "Incoming VPN connection (IPv6)"
+      from_port        = 443
+      to_port          = 443
+      protocol         = "udp"
+      #ipv6_cidr_blocks = module.vpc.private_subnets_ipv6_cidr_blocks
+      ipv6_cidr_blocks = module.vpc.vpc_ipv6_cidr_block
+    }
+  ]
+  number_of_computed_ingress_with_ipv6_cidr_blocks = 1
+
+  egress_with_cidr_blocks = [
+    {
+      description = "All"
+      from_port   = 0
+      to_port     = 0
+      protocol    = "-1"
+      cidr_blocks = "0.0.0.0/0"
+    }
+  ]
+
+  egress_with_ipv6_cidr_blocks = [
+    {
+      description = "All"
+      from_port   = 0
+      to_port     = 0
+      protocol    = "-1"
+      cidr_blocks = "2001:db8::/64"
+    }
+  ]
+
+  tags = {
+    Name        = "security_group"
+    Environment = "vpn"
+  }
+}
+
+resource "aws_ec2_client_vpn_endpoint" "vpn" {
+  description = "Omega Client VPN"
+  
+  client_cidr_block = "10.255.252.0/22"
+  split_tunnel = true
+  
+  server_certificate_arn = aws_acm_certificate_validation.vpn_server.certificate_arn
+
+  authentication_options {
+    type = "certificate-authentication"
+    root_certificate_chain_arn = aws_acm_certificate_validation.vpn_client_root.certificate_arn
+  }
+
+  connection_log_options {
+    enabled = true
+  }
+
+  tags = {
+    Name = "client_vpn_endpoint"
+    Environment = "vpn"
+  }
+}
+
+data "aws_subnet_ids" "vpc_subnet_ids" {
+  vpc_id = module.vpc.vpc_id
+}
+
+resource "aws_ec2_client_vpn_network_association" "vpn_subnets" {
+  count = 1
+
+  client_vpn_endpoint_id = aws_ec2_client_vpn_endpoint.vpn.id
+  subnet_id = tolist(data.aws_subnet_ids.vpc_subnet_ids.ids)[count.index]  #TODO(AR) is this the private or internal subnet
+  security_groups = [
+    module.vpn_access_security_group.security_group_id
+  ]
+
+  lifecycle {
+    // The issue why we are ignoring changes is that on every change
+    // terraform screws up most of the vpn assosciations
+    // see: https://github.com/hashicorp/terraform-provider-aws/issues/14717
+    ignore_changes = [subnet_id]
+  }
+}
+
+resource "aws_ec2_client_vpn_authorization_rule" "vpn_auth_rule" {
+  client_vpn_endpoint_id = aws_ec2_client_vpn_endpoint.vpn.id
+  # target_network_cidr = module.vpc.private_subnets_cidr_blocks  # TODO(AR) why can't we use private subnet here?
+  target_network_cidr = module.vpc.vpc_cidr_block
+  authorize_all_groups = true
 }
 
 module "vpc" {
@@ -70,12 +213,14 @@ module "vpc" {
   private_subnet_ipv6_prefixes    = [0]
   intra_subnet_ipv6_prefixes      = [16]
 
-  enable_nat_gateway  = true
-  single_nat_gateway  = false
-  reuse_nat_ips       = true             # <= Skip creation of EIPs for the NAT Gateways
-  external_nat_ip_ids = aws_eip.nat.*.id # <= IPs specified here as input to the module
+  # TODO (AR) are these needed still
+  #enable_nat_gateway  = true
+  #single_nat_gateway  = false
+  #reuse_nat_ips       = true             # <= Skip creation of EIPs for the NAT Gateways
+  #external_nat_ip_ids = aws_eip.nat.*.id # <= IPs specified here as input to the module
 
-  enable_vpn_gateway = true
+  enable_dns_hostnames = true
+  enable_dns_support = true
 
   tags = {
     Name = "vpc"
@@ -150,7 +295,7 @@ module "dev_workstation_secuirty_group" {
       description = "All"
       from_port   = 0
       to_port     = 0
-      protocol    = 0
+      protocol    = "-1"
       cidr_blocks = "0.0.0.0/0"
     }
   ]
@@ -160,7 +305,7 @@ module "dev_workstation_secuirty_group" {
       description = "All"
       from_port   = 0
       to_port     = 0
-      protocol    = 0
+      protocol    = "-1"
       cidr_blocks = "2001:db8::/64"
     }
   ]
@@ -178,7 +323,7 @@ resource "aws_network_interface" "dev_workstation_1_private_interface" {
   ipv6_address_count = 0 # use assign_ipv6_address_on_creation=true from the vpc subnet configuration
 
   security_groups = [
-      "dev_workstation_secuirty_group"
+      module.dev_workstation_secuirty_group.security_group_id
   ]
 
   tags = {
