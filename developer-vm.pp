38a78410581013bf4db11297571e6a5ec40f31c7
Adam Retter <adam.retter@googlemail.com>
2021-10-21 11:57:38 +0200

    Fix Java home

--- developer-vm.pp
+++ developer-vm.pp
@@ -39,11 +39,25 @@ exec { 'javac-11-as-default':
         ]
 }
 
+file { '/usr/lib/jvm/java':
+        ensure => link,
+        target => '/etc/alternatives/java_sdk',
+        replace => false,
+        owner => 'root',
+        group => 'root',
+        require => [
+                Package['java-11-amazon-corretto'],
+                Exec['java-11-as-default'],
+                Exec['javac-11-as-default']
+        ]
+}
+
 file { '/etc/profile.d/set-java-home.sh':
         content => 'export JAVA_HOME=/usr/lib/jvm/java',
         owner => 'root',
         group => 'root',
-        mode => '0644'
+        mode => '0644',
+        require => File['/usr/lib/jvm/java']
 }
 
 package { 'gcc':
