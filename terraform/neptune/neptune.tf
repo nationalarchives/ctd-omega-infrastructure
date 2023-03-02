4e7e6b61da56152f2b28f4d0ab55c5a743b44dbf
Adam Retter <adam.retter@googlemail.com>
2023-03-02 18:00:34 +0000

    Switch to severless, setup subnet, adjust attributes

--- terraform/neptune/neptune.tf
+++ terraform/neptune/neptune.tf
@@ -1,22 +1,64 @@
-resource "aws_neptune_cluster" "ctd-omega-neptune-cluster" {
-  cluster_identifier                  = "ctd-omega-neptune-cluster"
-  engine                              = "neptune"
-  engine_version                      = "1.0.4.0.neptune"
-  neptune_subnet_group_name       = "neptune-subnet-group"
-  backup_retention_period             = 5
-  preferred_backup_window             = "07:00-09:00"
-  skip_final_snapshot                 = true
+resource "aws_neptune_cluster" "ctd-omega-neptune-dev-cluster-a" {
+  cluster_identifier = "ctd-omega-neptune-dev-cluster-a"
+  availability_zones = local.aws_azs
+
+  neptune_subnet_group_name = "ctd-omega-neptune-dev-cluster-a-subnet-group"
+
+  serverless_v2_scaling_configuration {
+    min_capacity = 2.5
+    max_capacity = 12
+  }
+
   iam_database_authentication_enabled = true
-  apply_immediately                   = true
-  storage_encrypted               = true
-  replication_source_identifier   = "neptune-cluster-source"
-  snapshot_identifier             = "neptune-cluster-snapshot"
+  storage_encrypted                   = false
+
+  preferred_backup_window = "00:00-04:00"
+  backup_retention_period = 14 # days
+
+  preferred_maintenance_window = "04:00-08:00"
+
+  skip_final_snapshot       = false
+  final_snapshot_identifier = "ctd-omega-neptune-dev-cluster-a-final-snapshot"
+  copy_tags_to_snapshot     = true
+
+  apply_immediately   = true  # TODO(AR) should be set to false for production
+  deletion_protection = false # TODO(AR) should be set to true for production
+
+  tags = {
+    Name        = "ctd-omega-neptune-cluster-a"
+    Type        = "database"
+    DBType      = "neptune"
+    Environment = "dev"
+  }
 }
 
-resource "aws_neptune_cluster_instance" "ctd-omega-writer-instance" {
-  count              = 2
-  cluster_identifier = aws_neptune_cluster.ctd-omega-neptune-cluster.id
-  engine             = "neptune"
-  instance_class     = "db.t3.medium"
-  apply_immediately  = true
+resource "aws_neptune_cluster_instance" "ctd-omega-neptune-dev-cluster-a-instance" {
+  identifier_prefix  = "ctd-omega-neptune-dev-cluster-a-instance"
+  cluster_identifier = aws_neptune_cluster.ctd-omega-neptune-dev-cluster-a.id
+
+  count          = 2
+  instance_class = "db.serverless"
+
+  publicly_accessible       = false
+  neptune_subnet_group_name = "ctd-omega-neptune-dev-cluster-a-subnet-group"
+
+  apply_immediately = true # TODO(AR) should be set to false for production
+
+  tags = {
+    Name        = id
+    Type        = "database"
+    DBType      = "neptune"
+    Environment = "dev"
+  }
+}
+
+resource "aws_neptune_subnet_group" "ctd-omega-neptune-dev-cluster-a-subnet-group" {
+  name       = "main"
+  subnet_ids = [local.vpc_private_subnet_dev_databases]
+
+  tags = {
+    Name        = "ctd-omega-neptune-dev-cluster-a-subnet-group"
+    Type        = "neptune_subnet_group"
+    Environment = "dev"
+  }
 }
