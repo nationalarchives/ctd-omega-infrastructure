df11d61d13c858ebc44e0866b03cef461b70f30d
Jay Thomas <jason.thomas2@bp.com>
2023-07-12 19:50:39 +0100

    PACT-1045: Adding dev workstations and mssql server network interfaces and route53 records

--- terraform/environments/new/ec2_network_interfaces.tf
+++ terraform/environments/new/ec2_network_interfaces.tf
@@ -33,7 +33,7 @@ resource "aws_network_interface" "services_api_1_private_interface" {
   ipv6_address_count = 0 # use assign_ipv6_address_on_creation=true from the vpc subnet configuration
 
   tags = {
-    Name        = "eth0_omg_srevices_api_1"
+    Name        = "eth0_omg_services_api_1"
     Type        = "primary_network_interface"
     Network     = "omg_private"
     Environment = "omg"
@@ -47,7 +47,7 @@ resource "aws_network_interface" "puppet_server_1_private_interface" {
   ipv6_address_count = 0 # use assign_ipv6_address_on_creation=true from the vpc subnet configuration
 
   tags = {
-    Name        = "eth0_omg_srevices_api_1"
+    Name        = "eth0_omg_puppet_server_1"
     Type        = "primary_network_interface"
     Network     = "omg_private"
     Environment = "omg"
@@ -64,4 +64,70 @@ data "aws_network_interface" "web_app_1_private_interface" {
 
 data "aws_network_interface" "services_api_1_private_interface" {
   id = aws_network_interface.services_api_1_private_interface.id
+}
+
+data "aws_network_interface" "puppet_server_1_private_interface" {
+  id = aws_network_interface.puppet_server_1_private_interface.id
+}
+
+resource "aws_network_interface" "dev_workstation_1_private_interface" {
+  description        = "Private Dev General Subnet Interface for Dev Workstation 1"
+  subnet_id          = module.vpc.private_subnets[0]
+  private_ips        = ["172.27.64.4"]
+  ipv6_address_count = 0 # use assign_ipv6_address_on_creation=true from the vpc subnet configuration
+
+  security_groups = [
+    module.dev_workstation_security_group.security_group_id
+  ]
+
+  tags = {
+    Name        = "eth0_dev1"
+    Type        = "primary_network_interface"
+    Network     = "dev_private"
+    Environment = "dev"
+  }
+}
+
+resource "aws_network_interface" "dev_workstation_2_private_interface" {
+  description        = "Private Dev General Subnet Interface for Dev Workstation 2"
+  subnet_id          = module.vpc.private_subnets[0]
+  private_ips        = ["172.27.64.5"]
+  ipv6_address_count = 0 # use assign_ipv6_address_on_creation=true from the vpc subnet configuration
+
+  security_groups = [
+    module.dev_workstation_security_group.security_group_id
+  ]
+
+  tags = {
+    Name        = "eth0_dev2"
+    Type        = "primary_network_interface"
+    Network     = "dev_private"
+    Environment = "dev"
+  }
+}
+
+resource "aws_network_interface" "dev_mssql_server_1_database_interface" {
+  description        = "Private Dev Database Subnet Interface for Dev MS SQL Server 1"
+  subnet_id          = module.vpc.database_subnets[0]
+  private_ips        = ["172.27.65.132"]
+  ipv6_address_count = 0 # use assign_ipv6_address_on_creation=true from the vpc subnet configuration
+
+  tags = {
+    Name        = "eth0_mssql1"
+    Type        = "primary_network_interface"
+    Network     = "dev_database"
+    Environment = "dev"
+  }
+}
+
+data "aws_network_interface" "dev_workstation_1_private_interface" {
+  id = aws_network_interface.dev_workstation_1_private_interface.id
+}
+
+data "aws_network_interface" "dev_workstation_2_private_interface" {
+  id = aws_network_interface.dev_workstation_2_private_interface.id
+}
+
+data "aws_network_interface" "dev_mssql_server_1_database_interface" {
+  id = aws_network_interface.dev_mssql_server_1_database_interface.id
 }
--- terraform/environments/new/route53.tf
+++ terraform/environments/new/route53.tf
@@ -19,3 +19,76 @@ output "omega_public_dns_servers" {
   description = "Public DNS Servers for Omega"
   value       = aws_route53_zone.omega_public_dns.name_servers
 }
+
+resource "aws_route53_zone" "omega_private_omg_dns" {
+  name = local.private_omg_dns_domain
+
+  vpc {
+    vpc_id = module.vpc.vpc_id
+  }
+
+  tags = {
+    name = "dns_zone"
+  }
+}
+
+output "omega_private_omg_dns_servers" {
+  description = "DNS Servers for Omega internal environment"
+  value       = aws_route53_zone.omega_private_omg_dns.name_servers
+}
+
+resource "aws_route53_record" "dns_a_web_proxy_1_in_omg_catalogue_nationalarchives_gov_uk" {
+  zone_id = aws_route53_zone.omega_private_omg_dns.zone_id
+  name    = "web-proxy-1.${local.private_omg_dns_domain}"
+  type    = "A"
+  ttl     = "300"
+  records = data.aws_network_interface.web_proxy_1_private_interface.private_ips
+}
+
+resource "aws_route53_record" "dns_a_web_app_1_in_omg_catalogue_nationalarchives_gov_uk" {
+  zone_id = aws_route53_zone.omega_private_omg_dns.zone_id
+  name    = "web-app-1.${local.private_omg_dns_domain}"
+  type    = "A"
+  ttl     = "300"
+  records = data.aws_network_interface.web_app_1_private_interface.private_ips
+}
+
+resource "aws_route53_record" "dns_a_services_api_1_in_omg_catalogue_nationalarchives_gov_uk" {
+  zone_id = aws_route53_zone.omega_private_omg_dns.zone_id
+  name    = "services-api-1.${local.private_omg_dns_domain}"
+  type    = "A"
+  ttl     = "300"
+  records = data.aws_network_interface.services_api_1_private_interface.private_ips
+}
+
+resource "aws_route53_record" "dns_a_puppet_server_1_in_omg_catalogue_nationalarchives_gov_uk" {
+  zone_id = aws_route53_zone.omega_private_omg_dns.zone_id
+  name    = "puppet-server-1.${local.private_omg_dns_domain}"
+  type    = "A"
+  ttl     = "300"
+  records = data.aws_network_interface.puppet_server_1_private_interface.private_ips
+}
+
+resource "aws_route53_record" "dns_a_dev1_in_omg_catalogue_nationalarchives_gov_uk" {
+  zone_id = aws_route53_zone.omega_private_omg_dns.zone_id
+  name    = "dev1.${local.private_omg_dns_domain}"
+  type    = "A"
+  ttl     = "300"
+  records = data.aws_network_interface.dev_workstation_1_private_interface.private_ips
+}
+
+resource "aws_route53_record" "dns_a_dev2_in_omg_catalogue_nationalarchives_gov_uk" {
+  zone_id = aws_route53_zone.omega_private_omg_dns.zone_id
+  name    = "dev2.${local.private_omg_dns_domain}"
+  type    = "A"
+  ttl     = "300"
+  records = data.aws_network_interface.dev_workstation_2_private_interface.private_ips
+}
+
+resource "aws_route53_record" "dns_a_mssql1_in_omg_catalogue_nationalarchives_gov_uk" {
+  zone_id = aws_route53_zone.omega_private_omg_dns.zone_id
+  name    = "mssql1.${local.private_omg_dns_domain}"
+  type    = "A"
+  ttl     = "300"
+  records = data.aws_network_interface.dev_mssql_server_1_database_interface.private_ips
+}
