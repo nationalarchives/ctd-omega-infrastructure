63759f73833b19e0c019b051da6368751d783f6d
Jay Thomas <jason.thomas2@bp.com>
2023-07-12 13:29:58 +0100

    PACT-1041: interface for web-app-1 created

--- terraform/environments/new/ec2_network_interfaces.tf
+++ terraform/environments/new/ec2_network_interfaces.tf
@@ -1,17 +1,25 @@
 resource "aws_network_interface" "web_proxy_1_private_interface" {
-  description        = "Private Subnet TNA network access for MVP Beta Interface for Dev Workstation 2"
+  description        = "Private MVP Beta TNA Network Access Subnet interface for web-proxy-1"
   subnet_id          = module.vpc.private_subnets[8] # 9 of 10
   private_ips        = ["172.28.0.4"] # ["10.128.200.4"] ## private mvp beta tna access subnet AZ=2a 172.28.0.0/27
   ipv6_address_count = 0 # use assign_ipv6_address_on_creation=true from the vpc subnet configuration
 
-/*
-  security_groups = [
-    module.dev_workstation_security_group.security_group_id
-  ]
-*/
+  tags = {
+    Name        = "eth0_omg_web_proxy_1"
+    Type        = "primary_network_interface"
+    Network     = "omg_private"
+    Environment = "omg"
+  }
+}
+
+resource "aws_network_interface" "web_app_1_private_interface" {
+  description        = "Private MVP Beta Web Subnet Interface for web-app-1"
+  subnet_id          = module.vpc.private_subnets[4] # {5 of 10}
+  private_ips        = ["172.27.1.4"] # private mvp beta web subnet AZ=2a 172.27.1.0/24
+  ipv6_address_count = 0 # use assign_ipv6_address_on_creation=true from the vpc subnet configuration
 
   tags = {
-    Name        = "eth0_omg_webproxy"
+    Name        = "eth0_omg_web_app_1"
     Type        = "primary_network_interface"
     Network     = "omg_private"
     Environment = "omg"
@@ -20,6 +28,10 @@ resource "aws_network_interface" "web_proxy_1_private_interface" {
 
 data "aws_network_interface" "web_proxy_1_private_interface" {
   id = aws_network_interface.web_proxy_1_private_interface.id
+}
+
+data "aws_network_interface" "web_app_1_private_interface" {
+  id = aws_network_interface.web_app_1_private_interface.id
 }
 
 /*
