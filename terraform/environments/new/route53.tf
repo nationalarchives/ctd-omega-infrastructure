bfc8a45d6595bebd167895008691672899317405
Jay Thomas <jason.thomas2@bp.com>
2023-07-13 10:46:20 +0100

    PACT-1045: add route53 records for www and wildcard in the public domain.

--- terraform/environments/new/route53.tf
+++ terraform/environments/new/route53.tf
@@ -20,6 +20,22 @@ output "omega_public_dns_servers" {
   value       = aws_route53_zone.omega_public_dns.name_servers
 }
 
+resource "aws_route53_record" "dns_a_wildcard_in_catalogue_nationalarchives_gov_uk" {
+  zone_id = aws_route53_zone.omega_public_dns.zone_id
+  name    = "*.${local.public_dns_domain}"
+  type    = "A"
+  ttl     = "300"
+  records = data.aws_network_interface.web_proxy_1_private_interface.private_ips
+}
+
+resource "aws_route53_record" "dns_a_www_in_catalogue_nationalarchives_gov_uk" {
+  zone_id = aws_route53_zone.omega_public_dns.zone_id
+  name    = "www.${local.public_dns_domain}"
+  type    = "A"
+  ttl     = "300"
+  records = data.aws_network_interface.web_proxy_1_private_interface.private_ips
+}
+
 resource "aws_route53_zone" "omega_private_omg_dns" {
   name = local.private_omg_dns_domain
 
