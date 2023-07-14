ff1a3b2c429583319547e057f3b843ff61c7e98f
Jay Thomas <jason.thomas2@bp.com>
2023-07-14 23:06:15 +0100

    PACT-1014: Add vpn client root ca certificate and vpn server acm certificate with dns validation.

--- terraform/environments/new/route53.tf
+++ terraform/environments/new/route53.tf
@@ -20,6 +20,7 @@ output "omega_public_dns_servers" {
   value       = aws_route53_zone.omega_public_dns.name_servers
 }
 
+/*
 resource "aws_route53_record" "dns_a_wildcard_in_catalogue_nationalarchives_gov_uk" {
   zone_id = aws_route53_zone.omega_public_dns.zone_id
   name    = "*.${local.public_dns_domain}"
@@ -27,6 +28,7 @@ resource "aws_route53_record" "dns_a_wildcard_in_catalogue_nationalarchives_gov_
   ttl     = "300"
   records = data.aws_network_interface.web_proxy_1_private_interface.private_ips
 }
+*/
 
 resource "aws_route53_record" "dns_a_www_in_catalogue_nationalarchives_gov_uk" {
   zone_id = aws_route53_zone.omega_public_dns.zone_id
