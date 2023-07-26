dc989c245e0c7b5188c09f751d2cb3682fe36666
Jay Thomas <jason.thomas2@bp.com>
2023-07-26 17:50:46 +0100

    PACT-1083: Add terraform config for the Dev2 EC2 instance

--- terraform/environments/new/ec2_ebs_volumes.tf
+++ terraform/environments/new/ec2_ebs_volumes.tf
@@ -19,8 +19,35 @@ resource "aws_ebs_volume" "dev_workstation_1_home_volume" {
 */
 }
 
+resource "aws_ebs_volume" "dev_workstation_2_home_volume" {
+  availability_zone = "eu-west-2a"
+  encrypted         = false
+  iops              = 3000
+  type              = "gp3"
+  throughput        = 125
+  size              = 200
+
+  tags = {
+    Name        = "home_dev2_new"
+    Type        = "home_volume"
+    Environment = "dev"
+  }
+
+  /* # Uncomment to enable this argument when the environment is active. 
+  lifecycle {
+        prevent_destroy = true
+  }
+*/
+}
+
 resource "aws_volume_attachment" "dev_workstation_1_home_volume_ebs_att" {
   device_name = "/dev/xvdb"
   volume_id   = aws_ebs_volume.dev_workstation_1_home_volume.id
   instance_id = aws_instance.dev_workstation_1.id
+}
+
+resource "aws_volume_attachment" "dev_workstation_2_home_volume_ebs_att" {
+  device_name = "/dev/xvdb"
+  volume_id   = aws_ebs_volume.dev_workstation_2_home_volume.id
+  instance_id = aws_instance.dev_workstation_2.id
 }
