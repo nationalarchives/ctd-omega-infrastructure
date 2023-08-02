9e97a53252d049c98d133f838436ddadd04ef440
Adam Retter <adam.retter@googlemail.com>
2023-08-02 15:40:01 +0200

    Update to newer certificates

--- terraform/environments/new/client_vpn_client_ca_certificate.tf
+++ terraform/environments/new/client_vpn_client_ca_certificate.tf
@@ -1,27 +1,27 @@
-data "local_sensitive_file" "project_omega_private_vpn_client_ca_private_key_new" {
-  filename = ("../../../ctd-omega-infrastructure-certificates/exported/project-omega-private-vpn-client-ca.private.key.pem")
+data "local_sensitive_file" "project_omega_private_client_vpn_ca_private_key_new" {
+  filename = ("../../../ctd-omega-infrastructure-certificates/exported/project-omega-private-client-vpn-ca.private.key.pem")
 }
 
-data "tls_certificate" "project_omega_private_vpn_client_ca_chain_certificate_new" {
-  content = file("../../../ctd-omega-infrastructure-certificates/exported/project-omega-private-vpn-client-ca.crt.chain.pem")
+data "tls_certificate" "project_omega_private_client_vpn_ca_chain_certificate_new" {
+  content = file("../../../ctd-omega-infrastructure-certificates/exported/project-omega-private-client-vpn-ca.crt.chain.pem")
 }
 
-data "tls_certificate" "project_omega_private_vpn_client_ca_certificate_new" {
-  content = file("../../../ctd-omega-infrastructure-certificates/exported/project-omega-private-vpn-client-ca.crt.pem")
+data "tls_certificate" "project_omega_private_client_vpn_ca_certificate_new" {
+  content = file("../../../ctd-omega-infrastructure-certificates/exported/project-omega-private-client-vpn-ca.crt.pem")
 }
 
-resource "aws_acm_certificate" "vpn_client_ca_certificate_new" {
-  private_key       = data.local_sensitive_file.project_omega_private_vpn_client_ca_private_key_new.content
-  certificate_chain = data.tls_certificate.project_omega_private_vpn_client_ca_chain_certificate_new.content
-  certificate_body  = data.tls_certificate.project_omega_private_vpn_client_ca_certificate_new.content
+resource "aws_acm_certificate" "client_vpn_ca_certificate_new" {
+  private_key       = data.local_sensitive_file.project_omega_private_client_vpn_ca_private_key_new.content
+  certificate_chain = data.tls_certificate.project_omega_private_client_vpn_ca_chain_certificate_new.content
+  certificate_body  = data.tls_certificate.project_omega_private_client_vpn_ca_certificate_new.content
 
   lifecycle {
     create_before_destroy = true
   }
 
   tags = {
-    Name        = "vpn_client_ca_certificate_new"
-    Scope       = "vpn-client-ca-certificate-new"
+    Name        = "client_vpn_ca_certificate_new"
+    Scope       = "client-vpn-ca-certificate-new"
     Environment = "vpn"
   }
 }
