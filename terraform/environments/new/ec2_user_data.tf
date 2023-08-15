6c0c25d1201915c9acb81b7ca8f87172d36d4c61
Adam Retter <adam.retter@googlemail.com>
2023-08-15 10:51:27 -0400

    Tidy up cloud-init configs

--- terraform/environments/new/ec2_user_data.tf
+++ terraform/environments/new/ec2_user_data.tf
@@ -3,22 +3,13 @@ data "cloudinit_config" "dev_workstation_new" {
   base64_encode = true
 
   part {
-    content_type = "text/cloud-config"
-    filename     = "omega-mount-volumes.yaml"
-    content      = <<EOF
-#cloud-config
-mounts:
- - [ "/dev/xvdb", "/home", "xfs", "defaults,nofail", "0", "0" ]
-EOF
-  }
-
-  part {
     content_type = "text/x-shellscript"
-    filename     = "omega-01-format-and-mount-volumes.sh"
+    filename     = "omega-01-prepare-home-volume.sh"
     content      = <<EOF
 #!/usr/bin/env bash
-mkfs -t xfs /dev/xvdb
+set -e
 cp -rp /home/ec2-user /tmp
+mkfs -t xfs /dev/xvdb
 mount /dev/xvdb /home
 mv -f /tmp/ec2-user /home
 HOME_VOLUME_UUID=$(blkid /dev/xvdb | sed -n 's/.*UUID=\"\([^\"]*\)\".*/\1/p')
@@ -28,11 +19,34 @@ EOF
 
   part {
     content_type = "text/cloud-config"
-    filename     = "yum-upgrade.yaml"
+    filename     = "omega-02-mount-home-volume.yaml"
+    content      = <<EOF
+#cloud-config
+mounts:
+ - [ "/dev/xvdb", "/home", "xfs", "defaults,nofail", "0", "0" ]
+EOF
+  }
+
+  part {
+    content_type = "text/cloud-config"
+    filename     = "omega-03-yum-upgrade.yaml"
     content      = <<EOF
 #cloud-config
 package_update: true
 package_upgrade: true
+package_reboot_if_required: false
+EOF
+  }
+
+  part {
+    content_type = "text/cloud-config"
+    filename     = "omega-04-reboot.yaml"
+    content      = <<EOF
+#cloud-config
+power_state:
+    delay: now
+    mode: reboot
+    message: Rebooting machine after Omega cloud-init initialisation completed
 EOF
   }
 }
@@ -42,32 +56,11 @@ data "cloudinit_config" "mssql_server_new" {
   base64_encode = true
 
   part {
-    content_type = "text/cloud-config"
-    filename     = "omega-mount-volumes.yaml"
-    content      = <<EOF
-#cloud-config
-mounts:
- - [ xvdb, /mssql/data, "xfs", "defaults,nofail", "0", "0" ]
- - [ xvdc, /mssql/log, "xfs", "defaults,nofail", "0", "0" ]
- - [ xvdd, /mssql/backup, "xfs", "defaults,nofail", "0", "0" ]
-EOF
-  }
-
-  part {
-    content_type = "text/cloud-config"
-    filename     = "omega-yum-upgrade.yaml"
-    content      = <<EOF
-#cloud-config
-package_update: true
-package_upgrade: true
-EOF
-  }
-
-  part {
     content_type = "text/x-shellscript"
-    filename     = "omega-01-format-and-mount-volumes.sh"
+    filename     = "omega-01-prepare-volumes.sh"
     content      = <<EOF
 #!/usr/bin/env bash
+set -e
 mkfs -t xfs /dev/xvdb
 mkfs -t xfs /dev/xvdc
 mkfs -t xfs /dev/xvdd
@@ -80,13 +73,36 @@ EOF
 
   part {
     content_type = "text/cloud-config"
-    filename     = "reboot.yaml"
+    filename     = "omega-02-mount-volumes.yaml"
+    content      = <<EOF
+#cloud-config
+mounts:
+ - [ xvdb, /mssql/data, "xfs", "defaults,nofail", "0", "0" ]
+ - [ xvdc, /mssql/log, "xfs", "defaults,nofail", "0", "0" ]
+ - [ xvdd, /mssql/backup, "xfs", "defaults,nofail", "0", "0" ]
+EOF
+  }
+
+  part {
+    content_type = "text/cloud-config"
+    filename     = "omega-03-yum-upgrade.yaml"
+    content      = <<EOF
+#cloud-config
+package_update: true
+package_upgrade: true
+package_reboot_if_required: false
+EOF
+  }
+
+  part {
+    content_type = "text/cloud-config"
+    filename     = "omega-04-reboot.yaml"
     content      = <<EOF
 #cloud-config
 power_state:
     delay: now
     mode: reboot
-    message: Rebooting machine after Omega cloud-init Initialisation Completed
+    message: Rebooting machine after Omega cloud-init initialisation completed
 EOF
   }
 }
