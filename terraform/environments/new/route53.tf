adf4a5129507fa3eedf4176504263f266cfa2764
Adam Retter <adam.retter@googlemail.com>
2023-08-14 22:23:34 -0400

    Add records to IPv6 reverse zone

--- terraform/environments/new/route53.tf
+++ terraform/environments/new/route53.tf
@@ -138,6 +138,20 @@ resource "aws_route53_record" "dns_ipv4_ptr_web_proxy_1_in_omg_catalogue_nationa
   records = [aws_route53_record.dns_a_web_proxy_1_in_omg_catalogue_nationalarchives_gov_uk.name]
 }
 
+module "web_proxy_1_reverse_ipv6_address" {
+  source = "./reverse-ipv6-address"
+
+  ipv6_address = tolist(data.aws_network_interface.web_proxy_1_private_interface.ipv6_addresses)[0]
+}
+
+resource "aws_route53_record" "dns_ipv6_ptr_web_proxy_1_in_omg_catalogue_nationalarchives_gov_uk" {
+  zone_id = aws_route53_zone.omega_private_ipv6_omg_reverse_dns.zone_id
+  name = module.web_proxy_1_reverse_ipv6_address.reverse_dns_zone_name
+  type    = "PTR"
+  ttl     = "300"
+  records = [aws_route53_record.dns_aaaa_web_proxy_1_in_omg_catalogue_nationalarchives_gov_uk.name]
+}
+
 resource "aws_route53_record" "dns_a_web_app_1_in_omg_catalogue_nationalarchives_gov_uk" {
   zone_id = aws_route53_zone.omega_private_omg_dns.zone_id
   name    = "web-app-1.${local.private_omg_dns_domain}"
@@ -166,6 +180,20 @@ resource "aws_route53_record" "dns_ipv4_ptr_web_app_1_in_omg_catalogue_nationala
   type    = "PTR"
   ttl     = "300"
   records = [aws_route53_record.dns_a_web_app_1_in_omg_catalogue_nationalarchives_gov_uk.name]
+}
+
+module "web_app_1_reverse_ipv6_address" {
+  source = "./reverse-ipv6-address"
+
+  ipv6_address = tolist(data.aws_network_interface.web_app_1_private_interface.ipv6_addresses)[0]
+}
+
+resource "aws_route53_record" "dns_ipv6_ptr_web_app_1_in_omg_catalogue_nationalarchives_gov_uk" {
+  zone_id = aws_route53_zone.omega_private_ipv6_omg_reverse_dns.zone_id
+  name = module.web_app_1_reverse_ipv6_address.reverse_dns_zone_name
+  type    = "PTR"
+  ttl     = "300"
+  records = [aws_route53_record.dns_aaaa_web_app_1_in_omg_catalogue_nationalarchives_gov_uk.name]
 }
 
 resource "aws_route53_record" "dns_a_services_api_1_in_omg_catalogue_nationalarchives_gov_uk" {
@@ -198,6 +226,20 @@ resource "aws_route53_record" "dns_ipv4_ptr_services_api_1_in_omg_catalogue_nati
   records = [aws_route53_record.dns_a_services_api_1_in_omg_catalogue_nationalarchives_gov_uk.name]
 }
 
+module "services_api_1_reverse_ipv6_address" {
+  source = "./reverse-ipv6-address"
+
+  ipv6_address = tolist(data.aws_network_interface.services_api_1_private_interface.ipv6_addresses)[0]
+}
+
+resource "aws_route53_record" "dns_ipv6_ptr_services_api_1_in_omg_catalogue_nationalarchives_gov_uk" {
+  zone_id = aws_route53_zone.omega_private_ipv6_omg_reverse_dns.zone_id
+  name = module.services_api_1_reverse_ipv6_address.reverse_dns_zone_name
+  type    = "PTR"
+  ttl     = "300"
+  records = [aws_route53_record.dns_aaaa_services_api_1_in_omg_catalogue_nationalarchives_gov_uk.name]
+}
+
 resource "aws_route53_record" "dns_a_puppet_server_1_in_omg_catalogue_nationalarchives_gov_uk" {
   zone_id = aws_route53_zone.omega_private_omg_dns.zone_id
   name    = "puppet-server-1.${local.private_omg_dns_domain}"
@@ -226,6 +268,20 @@ resource "aws_route53_record" "dns_ipv4_ptr_puppet_server_1_in_omg_catalogue_nat
   type    = "PTR"
   ttl     = "300"
   records = [aws_route53_record.dns_a_puppet_server_1_in_omg_catalogue_nationalarchives_gov_uk.name]
+}
+
+module "puppet_server_1_reverse_ipv6_address" {
+  source = "./reverse-ipv6-address"
+
+  ipv6_address = tolist(data.aws_network_interface.puppet_server_1_private_interface.ipv6_addresses)[0]
+}
+
+resource "aws_route53_record" "dns_ipv6_ptr_puppet_server_1_in_omg_catalogue_nationalarchives_gov_uk" {
+  zone_id = aws_route53_zone.omega_private_ipv6_omg_reverse_dns.zone_id
+  name = module.puppet_server_1_reverse_ipv6_address.reverse_dns_zone_name
+  type    = "PTR"
+  ttl     = "300"
+  records = [aws_route53_record.dns_aaaa_puppet_server_1_in_omg_catalogue_nationalarchives_gov_uk.name]
 }
 
 resource "aws_route53_record" "dns_a_dev_workstation_1_in_omg_catalogue_nationalarchives_gov_uk" {
@@ -258,6 +314,20 @@ resource "aws_route53_record" "dns_ipv4_ptr_dev_workstation_1_in_omg_catalogue_n
   records = [aws_route53_record.dns_a_dev_workstation_1_in_omg_catalogue_nationalarchives_gov_uk.name]
 }
 
+module "dev_workstation_1_reverse_ipv6_address" {
+  source = "./reverse-ipv6-address"
+
+  ipv6_address = tolist(data.aws_network_interface.dev_workstation_1_private_interface.ipv6_addresses)[0]
+}
+
+resource "aws_route53_record" "dns_ipv6_ptr_dev_workstation_1_in_omg_catalogue_nationalarchives_gov_uk" {
+  zone_id = aws_route53_zone.omega_private_ipv6_omg_reverse_dns.zone_id
+  name = module.dev_workstation_1_reverse_ipv6_address.reverse_dns_zone_name
+  type    = "PTR"
+  ttl     = "300"
+  records = [aws_route53_record.dns_aaaa_dev_workstation_1_in_omg_catalogue_nationalarchives_gov_uk.name]
+}
+
 resource "aws_route53_record" "dns_a_dev_workstation_2_in_omg_catalogue_nationalarchives_gov_uk" {
   zone_id = aws_route53_zone.omega_private_omg_dns.zone_id
   name    = "dev-workstation-2.${local.private_omg_dns_domain}"
@@ -288,6 +358,20 @@ resource "aws_route53_record" "dns_ipv4_ptr_dev_workstation_2_in_omg_catalogue_n
   records = [aws_route53_record.dns_a_dev_workstation_2_in_omg_catalogue_nationalarchives_gov_uk.name]
 }
 
+module "dev_workstation_2_reverse_ipv6_address" {
+  source = "./reverse-ipv6-address"
+
+  ipv6_address = tolist(data.aws_network_interface.dev_workstation_2_private_interface.ipv6_addresses)[0]
+}
+
+resource "aws_route53_record" "dns_ipv6_ptr_dev_workstation_2_in_omg_catalogue_nationalarchives_gov_uk" {
+  zone_id = aws_route53_zone.omega_private_ipv6_omg_reverse_dns.zone_id
+  name = module.dev_workstation_2_reverse_ipv6_address.reverse_dns_zone_name
+  type    = "PTR"
+  ttl     = "300"
+  records = [aws_route53_record.dns_aaaa_dev_workstation_2_in_omg_catalogue_nationalarchives_gov_uk.name]
+}
+
 resource "aws_route53_record" "dns_a_dev_mssql_server_1_in_omg_catalogue_nationalarchives_gov_uk" {
   zone_id = aws_route53_zone.omega_private_omg_dns.zone_id
   name    = "dev-mssql-server-1.${local.private_omg_dns_domain}"
@@ -316,4 +400,18 @@ resource "aws_route53_record" "dns_ipv4_ptr_dev_mssql_server_1_in_omg_catalogue_
   type    = "PTR"
   ttl     = "300"
   records = [aws_route53_record.dns_a_dev_mssql_server_1_in_omg_catalogue_nationalarchives_gov_uk.name]
+}
+
+module "dev_mssql_server_1_reverse_ipv6_address" {
+  source = "./reverse-ipv6-address"
+
+  ipv6_address = tolist(data.aws_network_interface.dev_mssql_server_1_database_interface.ipv6_addresses)[0]
+}
+
+resource "aws_route53_record" "dns_ipv6_ptr_dev_mssql_server_1_in_omg_catalogue_nationalarchives_gov_uk" {
+  zone_id = aws_route53_zone.omega_private_ipv6_omg_reverse_dns.zone_id
+  name = module.dev_mssql_server_1_reverse_ipv6_address.reverse_dns_zone_name
+  type    = "PTR"
+  ttl     = "300"
+  records = [aws_route53_record.dns_aaaa_dev_mssql_server_1_in_omg_catalogue_nationalarchives_gov_uk.name]
 }
