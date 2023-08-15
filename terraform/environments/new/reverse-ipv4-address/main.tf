7db0196a898c0f4a9bad649993656ac8a7f148dd
Adam Retter <adam.retter@googlemail.com>
2023-08-14 21:21:27 -0400

    Abstract Reverse IPv4 functions into a Module

---
+++ terraform/environments/new/reverse-ipv4-address/main.tf
@@ -0,0 +1,7 @@
+locals {
+    clean_ipv4_address = "${replace(var.ipv4_address, "/^([^/]+)(?:/[0-9]+)?/", "$1")}"
+
+    ipv4_address_list = split(".", local.clean_ipv4_address)
+
+    reverse_ipv4_address_list = reverse(local.ipv4_address_list)
+}
