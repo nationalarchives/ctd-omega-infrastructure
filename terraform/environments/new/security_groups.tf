eeadb3d0fd300f04bbfc67d5a6ea987bf1c6d490
Adam Retter <adam.retter@googlemail.com>
2023-08-22 09:10:52 -0400

    Allow SSH from vpc_private_subnet_dev_general to vpc_private_subnet_dev_databases

--- terraform/environments/new/security_groups.tf
+++ terraform/environments/new/security_groups.tf
@@ -97,3 +97,75 @@ module "dev_workstation_security_group" {
     Environment = "dev"
   }
 }
+
+module "dev_database_security_group" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "5.1.0"
+
+  name        = "dev_database_security_group_new"
+  description = "Security group for Development Database ports open within VPC"
+
+  vpc_id = module.vpc.vpc_id
+
+  computed_ingress_with_cidr_blocks = [
+    {
+      description = "SSH"
+      from_port   = 22
+      to_port     = 22
+      protocol    = "tcp"
+      cidr_blocks = module.vpc.database_subnets_cidr_blocks[0] # NOTE: restricted to vpc_private_subnet_dev_databases
+    },
+    {
+      description = "SSH from vpc_private_subnet_dev_general"
+      from_port   = 22
+      to_port     = 22
+      protocol    = "tcp"
+      cidr_blocks = module.vpc.private_subnets_cidr_blocks[0] # NOTE: restricted to vpc_private_subnet_dev_general
+    }
+  ]
+  number_of_computed_ingress_with_cidr_blocks = 2
+
+  computed_ingress_with_ipv6_cidr_blocks = [
+    {
+      description      = "SSH (IPv6)"
+      from_port        = 22
+      to_port          = 22
+      protocol         = "tcp"
+      ipv6_cidr_blocks = module.vpc.database_subnets_ipv6_cidr_blocks[0] # NOTE: restricted to vpc_private_subnet_dev_databases (IPv6)
+    },
+    {
+      description      = "SSH (IPv6) from vpc_private_subnet_dev_general"
+      from_port        = 22
+      to_port          = 22
+      protocol         = "tcp"
+      ipv6_cidr_blocks = module.vpc.private_subnets_ipv6_cidr_blocks[0] # NOTE: restricted to vpc_private_subnet_dev_general (IPv6)
+    }
+  ]
+  number_of_computed_ingress_with_ipv6_cidr_blocks = 2
+
+  egress_with_cidr_blocks = [
+    {
+      description = "All"
+      from_port   = -1
+      to_port     = -1
+      protocol    = -1
+      cidr_blocks = "0.0.0.0/0"
+    }
+  ]
+
+  egress_with_ipv6_cidr_blocks = [
+    {
+      description = "All (IPv6)"
+      from_port   = -1
+      to_port     = -1
+      protocol    = -1
+      cidr_blocks = "2001:db8::/64"
+    }
+  ]
+
+  tags = {
+    Name        = "sg_dev_database_new"
+    Type        = "security_group"
+    Environment = "dev"
+  }
+}
