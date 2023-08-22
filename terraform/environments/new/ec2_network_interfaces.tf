efac4ecc817eeb9752e18b683c39afdb59f33604
Adam Retter <adam.retter@googlemail.com>
2023-08-22 09:10:52 -0400

    Allow SSH from vpc_private_subnet_dev_general to vpc_private_subnet_dev_databases

--- terraform/environments/new/ec2_network_interfaces.tf
+++ terraform/environments/new/ec2_network_interfaces.tf
@@ -105,6 +105,10 @@ resource "aws_network_interface" "dev_mssql_server_1_database_interface" {
   subnet_id          = module.vpc.database_subnets[0]
   private_ips        = ["10.129.203.4"]
 
+  security_groups = [
+    module.dev_database_security_group.security_group_id
+  ]
+
   tags = {
     Name        = "eth0_dev-mssql-server1"
     Type        = "primary_network_interface"
