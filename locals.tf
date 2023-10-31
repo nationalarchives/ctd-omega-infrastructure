06ade9c13352d6501a88109592c0da2c2f3cf21b
Adam Retter <adam.retter@googlemail.com>
2023-10-31 19:26:19 +0100

    Fixate the numbering of the IPv6 subnets

--- locals.tf
+++ locals.tf
@@ -19,78 +19,134 @@ locals {
   /* Primary CIDR for Private Omega */
   vpc_cidr_block = "10.129.192.0/18"
 
+  /* Public Subnet for General Use (NAT Gateway etc.) */
+  vpc_public_subnet_general = {
+    ipv4          = ["10.129.192.0/24",        "10.129.212.0/24"]
+    # ipv6        = ["2a05:d01c:7:1a0e::/64",  "2a05:d01c:7:1a0f::/64"]
+    ipv6_prefixes = [14, 15]
+  }
+
   /* Private Subnet for General Development */
-  vpc_private_subnet_dev_general = ["10.129.202.0/24", "10.129.222.0/24"]
+  vpc_private_subnet_dev_general = {
+    ipv4          = ["10.129.202.0/24",        "10.129.222.0/24"]
+    # ipv6        = ["2a05:d01c:7:1a00::/64",  "2a05:d01c:7:1a01::/64"]
+    ipv6_prefixes = [0, 1]
+  }
 
   /* Private Subnet for databases used in Development */
-  vpc_private_subnet_dev_databases = ["10.129.203.0/24", "10.129.223.0/24"]
-
-  /* Public Subnet for General Use (NAT Gateway etc.) */
-  vpc_public_subnet_general = ["10.129.192.0/24", "10.129.212.0/24"]
+  vpc_private_subnet_dev_databases = {
+    ipv4          = ["10.129.203.0/24",        "10.129.223.0/24"]
+    # ipv6        = ["2a05:d01c:7:1a0a::/64",  "2a05:d01c:7:1a0b::/64"]
+    ipv6_prefixes = [10, 11]
+  }
 
   /* Private Subnet for MVP Beta Web Application */
-  vpc_private_subnet_mvpbeta_web = ["10.129.193.0/24", "10.129.213.0/24"]
+  vpc_private_subnet_mvpbeta_web = {
+    ipv4          = ["10.129.193.0/24",        "10.129.213.0/24"]
+    # ipv6        = ["2a05:d01c:7:1a04::/64",  "2a05:d01c:7:1a05::/64"]
+    ipv6_prefixes = [4, 5]
+  }
 
   /* Private Subnet for MVP Beta Services */
-  vpc_private_subnet_mvpbeta_services = ["10.129.194.0/24", "10.129.214.0/24"]
+  vpc_private_subnet_mvpbeta_services = {
+    ipv4          = ["10.129.194.0/24",        "10.129.214.0/24"]
+    # ipv6        = ["2a05:d01c:7:1a06::/64",  "2a05:d01c:7:1a07::/64"]
+    ipv6_prefixes = [6, 7]
+  }
 
   /* Intra Subnet for databases used in MVP Beta */
-  vpc_intra_subnet_mvpbeta_databases = ["10.129.196.0/24", "10.129.216.0/24"]
+  vpc_intra_subnet_mvpbeta_databases = {
+    ipv4          = ["10.129.196.0/24",        "10.129.216.0/24"]
+    # ipv6        = ["2a05:d01c:7:1a0c::/64",  "2a05:d01c:7:1a0d::/64"]
+    ipv6_prefixes = [12, 13]
+  }
 
   /* Private Subnet for TNA network access for MVP Beta */
-  vpc_private_tna_net_subnet_mvpbeta = ["10.129.199.0/24", "10.129.219.0/24"]
+  vpc_private_tna_net_subnet_mvpbeta = {
+    ipv4          = ["10.129.199.0/24",        "10.129.219.0/24"]
+    # ipv6        = ["2a05:d01c:7:1a08::/64",  "2a05:d01c:7:1a09::/64"]
+    ipv6_prefixes = [8, 9]
+  }
 
   /* Private Subnet for Management */
-  vpc_private_subnet_management = ["10.129.195.0/24", "10.129.215.0/24"]
+  vpc_private_subnet_management = {
+    ipv4          = ["10.129.195.0/24",        "10.129.215.0/24"]
+    # ipv6        = ["2a05:d01c:7:1a02::/64",  "2a05:d01c:7:1a03::/64"]
+    ipv6_prefixes = [2, 3]
+  }
 
   vpc_private_subnets = tolist(
     concat(
       /* Development private subnets */
-      local.vpc_private_subnet_dev_general,
+      local.vpc_private_subnet_dev_general.ipv4,
 
       /* Management private subnets */
-      local.vpc_private_subnet_management,
+      local.vpc_private_subnet_management.ipv4,
 
       /* MVP Beta private subnets */
-      local.vpc_private_subnet_mvpbeta_web,
-      local.vpc_private_subnet_mvpbeta_services,
-      local.vpc_private_tna_net_subnet_mvpbeta
+      local.vpc_private_subnet_mvpbeta_web.ipv4,
+      local.vpc_private_subnet_mvpbeta_services.ipv4,
+      local.vpc_private_tna_net_subnet_mvpbeta.ipv4
     )
   )
 
-  vpc_private_ipv6_subnets = [for i in local.vpc_private_subnets : index(local.vpc_private_subnets, i)]
+  vpc_private_ipv6_subnets =  tolist(
+    concat(
+      /* Development private subnets */
+      local.vpc_private_subnet_dev_general.ipv6_prefixes,
+
+      /* Management private subnets */
+      local.vpc_private_subnet_management.ipv6_prefixes,
+
+      /* MVP Beta private subnets */
+      local.vpc_private_subnet_mvpbeta_web.ipv6_prefixes,
+      local.vpc_private_subnet_mvpbeta_services.ipv6_prefixes,
+      local.vpc_private_tna_net_subnet_mvpbeta.ipv6_prefixes
+    )
+  )
 
   vpc_database_subnets = tolist(
     concat(
       /* Development database subnets */
-      local.vpc_private_subnet_dev_databases
-
-      /* MVP Beta database subnets */
+      local.vpc_private_subnet_dev_databases.ipv4
     )
   )
 
-  vpc_database_ipv6_subnets = [for i in local.vpc_database_subnets : length(local.vpc_private_subnets) + index(local.vpc_database_subnets, i)]
+  vpc_database_ipv6_subnets = tolist(
+    concat(
+      /* Development database subnets */
+      local.vpc_private_subnet_dev_databases.ipv6_prefixes
+    )
+  )
 
   vpc_intra_subnets = tolist(
     concat(
-      /* Development intra subnets */
-
       /* MVP Beta intra subnets */
-      local.vpc_intra_subnet_mvpbeta_databases
+      local.vpc_intra_subnet_mvpbeta_databases.ipv4
     )
   )
 
-  vpc_intra_ipv6_subnets = [for i in local.vpc_intra_subnets : length(local.vpc_private_subnets) + length(local.vpc_database_subnets) + index(local.vpc_intra_subnets, i)]
+  vpc_intra_ipv6_subnets = tolist(
+    concat(
+      /* MVP Beta intra subnets */
+      local.vpc_intra_subnet_mvpbeta_databases.ipv6_prefixes
+    )
+  )
 
   vpc_public_subnets = tolist(
     concat(
       /* General Use subnet (NAT Gateway etc.) */
-      local.vpc_public_subnet_general,
+      local.vpc_public_subnet_general.ipv4
     )
   )
 
   /* starts public ipv6 subnets after private ipv6 subnets */
-  vpc_public_ipv6_subnets = [for i in local.vpc_public_subnets : length(local.vpc_private_subnets) + length(local.vpc_database_subnets) + length(local.vpc_intra_subnets) + index(local.vpc_public_subnets, i)]
+  vpc_public_ipv6_subnets = tolist(
+    concat(
+      /* General Use subnet (NAT Gateway etc.) */
+      local.vpc_public_subnet_general.ipv6_prefixes
+    )
+  )
 
   # See https://datatracker.ietf.org/doc/html/rfc6056.html#section-2
   unpriviledged_port_start = 1024
